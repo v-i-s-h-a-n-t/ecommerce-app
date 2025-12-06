98fa441d69e9b2f694fc1cd4d89df813e90a22a1
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2025-12-06 14:35:05 +1200

    feat: add product details page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,17 @@
-export default function ProductsPage() {
-    return <div>products</div>
+import { ProductList } from "@/components/productlist";
+import { stripe } from "@/lib/stripe";
+
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  return (
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All Products
+      </h1>
+      <ProductList products={products.data} />
+    </div>
+  );
 }
